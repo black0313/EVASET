59736a4db05b07089f74017c0283d2dfad12e82f
User0313 <95623862+User0313@users.noreply.github.com>
2022-03-08 09:10:16 +0500

    xarajat turlari

--- src/components/SotibOlish/Sidebar/Xarajatlar/Qoshish/XarajatQoshish.js
+++ src/components/SotibOlish/Sidebar/Xarajatlar/Qoshish/XarajatQoshish.js
@@ -258,37 +258,37 @@ function XarajatQoshish({editXarajatlar,users, saveXarajatlar, match, getXarajat
                             <input type="number" value={input.amaldagisoliq} onChange={amaldagisoliq}
                                    className={'form-control'} placeholder={'savdo raqami'}/>
 
-                            <label className={'mt-5'} htmlFor={'lang'}>lang_v1.is_refend</label>
-                            <input onClick={toggle} itemID={'lang'} style={{width: '20px', height: '20px'}}
-                                   type="checkbox" id={'lang'}/>
+                            {/*<label className={'mt-5'} htmlFor={'lang'}>lang_v1.is_refend</label>*/}
+                            {/*<input onClick={toggle} itemID={'lang'} style={{width: '20px', height: '20px'}}*/}
+                            {/*       type="checkbox" id={'lang'}/>*/}
                         </div>
-                        <Modal isOpen={active} toggle={toggle}>
-                            <ModalHeader>
-                                Qoshimcha
-                            </ModalHeader>
-                            <ModalBody>
-                                <label htmlFor={'doim'}>Doim takrorlanadimi</label>
-                                <input type="checkbox" value={input.doimtakror} onChange={doimtakror} id={'doim'}
-                                       style={{width: '20px', height: '20px'}}/><br/>
-                                <label htmlFor={'xsana'} className={'mt-3'}>Xarajat sanasi</label>
-                                <div className={'d-flex'}>
-                                    <input type="text" className={'form-control'} value={input.xarajatsana}
-                                           onChange={xarajatsana}/>
-                                    <select name="" value={input.takrorkun} onChange={takrorkun}>
-                                        <option value="">Hafta</option>
-                                        <option value="">Oy</option>
-                                        <option value="">Yil</option>
-                                    </select>
-                                </div>
-                                <label htmlFor={'takror'}>Takrorlashlar soni</label>
-                                <input type="number" className={'form-control'} value={input.takrorsoni}
-                                       onChange={takrorsoni}/>
-                            </ModalBody>
-                            <ModalFooter>
-                                <button className={'btn btn-primary'}>Saqlash</button>
-                                <button className={'btn btn-primary'} onClick={toggle}>Chiqish</button>
-                            </ModalFooter>
-                        </Modal>
+                        {/*<Modal isOpen={active} toggle={toggle}>*/}
+                        {/*    <ModalHeader>*/}
+                        {/*        Qoshimcha*/}
+                        {/*    </ModalHeader>*/}
+                        {/*    <ModalBody>*/}
+                        {/*        <label htmlFor={'doim'}>Doim takrorlanadimi</label>*/}
+                        {/*        <input type="checkbox" value={input.doimtakror} onChange={doimtakror} id={'doim'}*/}
+                        {/*               style={{width: '20px', height: '20px'}}/><br/>*/}
+                        {/*        <label htmlFor={'xsana'} className={'mt-3'}>Xarajat sanasi</label>*/}
+                        {/*        <div className={'d-flex'}>*/}
+                        {/*            <input type="text" className={'form-control'} value={input.xarajatsana}*/}
+                        {/*                   onChange={xarajatsana}/>*/}
+                        {/*            <select name="" value={input.takrorkun} onChange={takrorkun}>*/}
+                        {/*                <option value="">Hafta</option>*/}
+                        {/*                <option value="">Oy</option>*/}
+                        {/*                <option value="">Yil</option>*/}
+                        {/*            </select>*/}
+                        {/*        </div>*/}
+                        {/*        <label htmlFor={'takror'}>Takrorlashlar soni</label>*/}
+                        {/*        <input type="number" className={'form-control'} value={input.takrorsoni}*/}
+                        {/*               onChange={takrorsoni}/>*/}
+                        {/*    </ModalBody>*/}
+                        {/*    <ModalFooter>*/}
+                        {/*        <button className={'btn btn-primary'}>Saqlash</button>*/}
+                        {/*        <button className={'btn btn-primary'} onClick={toggle}>Chiqish</button>*/}
+                        {/*    </ModalFooter>*/}
+                        {/*</Modal>*/}
                         <div className="col-4 col-sm-12">
                             <label htmlFor={'savOyna'}>Qisqa eslatma</label>
                             <input type="text" className={'form-control'} value={input.qisqaeslatma}
@@ -308,59 +308,59 @@ function XarajatQoshish({editXarajatlar,users, saveXarajatlar, match, getXarajat
                     </div>
                 </div>
 
-                <div className="col-12 mt-4 border p-4">
-                    <div className="row">
-                        <div className="col-md-6 col-sm-12 d-flex">
-                            <input type="text" className={'form-control'} value={input.maxsulotnomishtrix}
-                                   onChange={maxsulotnomishtrix}
-                                   placeholder={'mahsulot nomi yoki shtrix kodini yozing'}/>
-                            <h5 style={{fontSize: '32px', cursor: 'pointer'}}>+</h5>
-                        </div>
-                        <div className="table-responsive mb-2">
-                            <table className={'table mt-4'}>
-                                <thead>
-                                <tr>
-                                    <th>Mahsulot</th>
-                                    <th>Miqdori</th>
-                                    <th>Narxi</th>
-                                    <th>Discount</th>
-                                    <th>Jami</th>
-                                    <th>x</th>
-                                </tr>
-                                </thead>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                {/*<div className="col-12 mt-4 border p-4">*/}
+                {/*    <div className="row">*/}
+                {/*        <div className="col-md-6 col-sm-12 d-flex">*/}
+                {/*            <input type="text" className={'form-control'} value={input.maxsulotnomishtrix}*/}
+                {/*                   onChange={maxsulotnomishtrix}*/}
+                {/*                   placeholder={'mahsulot nomi yoki shtrix kodini yozing'}/>*/}
+                {/*            <h5 style={{fontSize: '32px', cursor: 'pointer'}}>+</h5>*/}
+                {/*        </div>*/}
+                {/*        <div className="table-responsive mb-2">*/}
+                {/*            <table className={'table mt-4'}>*/}
+                {/*                <thead>*/}
+                {/*                <tr>*/}
+                {/*                    <th>Mahsulot</th>*/}
+                {/*                    <th>Miqdori</th>*/}
+                {/*                    <th>Narxi</th>*/}
+                {/*                    <th>Discount</th>*/}
+                {/*                    <th>Jami</th>*/}
+                {/*                    <th>x</th>*/}
+                {/*                </tr>*/}
+                {/*                </thead>*/}
+                {/*            </table>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
 
-                <div className="col-12  mt-4 border p-4">
-                    <div className="row">
-                        <h5 className={'text-center mt-5'}>To`lov qilish</h5>
-                        <div className="col-6 col-sm-12">
-                            <label htmlFor={'avans'}>Avans 0 / To`lov so`mmasi</label>
-                            <input type="text" className={'form-control'} id={'avans'}/>
-                            <label className={'mt-3'} htmlFor={'tol'}>To`lov usuli</label>
-                            <select name="" id={'tol'} className={'form-control'} onChange={tulovusuli}
-                                    value={input.tulovusuli}>
-                                <option value="">Naqd</option>
-                                <option value="">Pastik</option>
-                                <option value="">Cheque</option>
-                                <option value="">Bank Transfer</option>
-                                <option value="">UzCard</option>
-                                <option value="">Humo</option>
-                            </select>
-                        </div>
-                        <div className="col-md-6 col-sm-12">
-                            <label htmlFor={'paid'}>Paid on</label>
-                            <input type="date" value={input.paidon} onChange={paidon} className={'form-control'}
-                                   id={'paid'}/>
-                            <label htmlFor={'area1'} className={'mt-2'}>Eslatma</label>
-                            <textarea name="" id={'area1'} cols="30" value={input.eslatma} onChange={eslatma}
-                                      className={'form-control'} rows="2">
-                            </textarea>
-                        </div>
-                    </div>
-                </div>
+                {/*<div className="col-12  mt-4 border p-4">*/}
+                {/*    <div className="row">*/}
+                {/*        <h5 className={'text-center mt-5'}>To`lov qilish</h5>*/}
+                {/*        <div className="col-6 col-sm-12">*/}
+                {/*            <label htmlFor={'avans'}>Avans 0 / To`lov so`mmasi</label>*/}
+                {/*            <input type="text" className={'form-control'} id={'avans'}/>*/}
+                {/*            <label className={'mt-3'} htmlFor={'tol'}>To`lov usuli</label>*/}
+                {/*            <select name="" id={'tol'} className={'form-control'} onChange={tulovusuli}*/}
+                {/*                    value={input.tulovusuli}>*/}
+                {/*                <option value="">Naqd</option>*/}
+                {/*                <option value="">Pastik</option>*/}
+                {/*                <option value="">Cheque</option>*/}
+                {/*                <option value="">Bank Transfer</option>*/}
+                {/*                <option value="">UzCard</option>*/}
+                {/*                <option value="">Humo</option>*/}
+                {/*            </select>*/}
+                {/*        </div>*/}
+                {/*        <div className="col-md-6 col-sm-12">*/}
+                {/*            <label htmlFor={'paid'}>Paid on</label>*/}
+                {/*            <input type="date" value={input.paidon} onChange={paidon} className={'form-control'}*/}
+                {/*                   id={'paid'}/>*/}
+                {/*            <label htmlFor={'area1'} className={'mt-2'}>Eslatma</label>*/}
+                {/*            <textarea name="" id={'area1'} cols="30" value={input.eslatma} onChange={eslatma}*/}
+                {/*                      className={'form-control'} rows="2">*/}
+                {/*            </textarea>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
                 <div className={'col-md-10 offset-1 mt-5 border p-4'}>
                     <h5>Qarz miqdori!: 0.00</h5>
                     <Link to={'/headerthird/xarajatRuyxati'}>
